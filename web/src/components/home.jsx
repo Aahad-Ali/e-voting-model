c3a26adc353866b090c0b6832dd11d5a566491de
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2023-06-20 20:21:09 +0100

    Add files via upload

---
+++ web/src/components/home.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import react from 'react'
+import Add  from "./add";
+
+
+
+const Home=()=> {
+
+
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  )
+}
+
+
+export default Home;
